a08e4f363925aee2a84c8513914e436e0bd9c4cd
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-02 19:21:22 -0500

    View Index and Details
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Index1.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Frontend.Pages.Pacientes
+{
+    public class Index1Model : PageModel
+    {
+        private readonly IRepositorioPaciente _repoPaciente;
+        public IEnumerable<Paciente> pacientes {get; set;}
+        public Index1Model(IRepositorioPaciente repoPaciente)
+        {
+            _repoPaciente= repoPaciente;
+        }
+        public void OnGet()
+        {
+            pacientes=_repoPaciente.GetAllPacientes();
+        }
+    }
+}
