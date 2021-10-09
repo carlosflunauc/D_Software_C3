be80903eabd1ea90714b4b90a0e7c6090b4ccf06
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-09 09:32:40 -0500

    crear vista asignar medico
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- HospiEnCasa.App.Frontend/Pages/Pacientes/AddMedico.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/AddMedico.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
 
-namespace HospiEnCasa.App.Frontend.Pages
+namespace HospiEnCasa.App.Frontend.Pages.Pacientes
 {
     public class AddMedicoModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPaciente _repoPaciente;
+        private readonly IRepositorioMedico _repoMedico;
+        public Paciente paciente {get; set;}
+        public IEnumerable<Medico> medicos {get; set;}
+
+        public AddMedicoModel(IRepositorioPaciente repositorioPaciente, IRepositorioMedico repoMedico)
         {
+            _repoPaciente= repoPaciente;
+            _repoMedico=repoMedico;
+        }
+        public void OnGet(int id)
+        {
+            paciente=_repoPaciente.GetPaciente(id);
+            medicos=_repoMedico.GetAllMedicos();
+        }
+        public IActionResult OnPost(int idPaciente, int idMedico)
+        {
+            _repoPaciente.AsignarMedico(idPaciente, idMedico);
+            return RedirectToPage("Details1",new{id=idPaciente});
         }
     }
 }
