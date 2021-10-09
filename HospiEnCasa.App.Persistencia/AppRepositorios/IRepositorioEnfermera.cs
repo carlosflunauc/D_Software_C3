7286043675d36399bea253db6d73df3cca37bd84
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-09 09:03:36 -0500

    IEnfermeras
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ HospiEnCasa.App.Persistencia/AppRepositorios/IRepositorioEnfermera.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Persistencia
+{
+    public interface IRepositorioEnfermera
+    {
+     IEnumerable<Enfermera> GetAllEnfermeras();    
+    }
+}
