af56c59cebd3e049167e7d3404c98c16742ae94f
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-12 08:31:43 -0500

    Create medico
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ HospiEnCasa.App.Frontend/Pages/Medicos/Create1.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Persistencia;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Frontend.Pages.Medicos
+{
+    public class Create1Model : PageModel
+    {
+        private readonly IRepositorioMedico _repoMedico;
+        public Medico medico {get; set;}
+        public Create1Model(IRepositorioMedico repoMedico)
+        {
+            _repoMedico = repoMedico;
+        }
+        public void OnGet()
+        {
+            medico = new Medico();
+        }
+        public IActionResult OnPost(Medico medico)
+        {
+            _repoMedico.AddMedico(medico);
+            return RedirectToPage("Index1");
+        }
+    }
+}
