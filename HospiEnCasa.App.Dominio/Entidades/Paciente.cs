39c89c37715081733403a8da11e39f830f679511
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-12 09:12:52 -0500

    campos requeridos en entidades
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- HospiEnCasa.App.Dominio/Entidades/Paciente.cs
+++ HospiEnCasa.App.Dominio/Entidades/Paciente.cs
@@ -1,18 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
+    ///<summary>Class <c>Paciente</c>
+    /// Modelando el paciente que estan atendiendo en el Hospital
+    ///</summary>
     public class Paciente : Persona
     {
         public string Direccion { get; set; }
         public float Latitud { get; set; }
         public float Longitud { get; set; }
         public string Ciudad { get; set; }
+        [Display(Name="Fecha de Nacimiento")]
         public DateTime FechaNacimiento { get; set; }
         public FamiliarDesignado Familiar { get; set; }
         public Enfermera Enfermera { get; set; }
         public Medico Medico { get; set; }
         public Historia Historia { get; set; }
+        ///<summary>
+        ///Referencia a la lista de signos vitales de un paciente
+        ///</summary>
+        ///<value></value>
         public List<SignoVital> SignosVitales { get; set; }
     }
 }
--- HospiEnCasa.App.Dominio/Entidades/Persona.cs
+++ HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
+    ///<summary>Class <c>Persona</c>
+    /// Modelando una persona en el sistema
+    ///</summary>
     public class Persona
     {
         // Identificador único de cada persona
         public int Id { get; set; }
+        [Required(ErrorMessage="Campo Obligatorio")]
+        [StringLength(50, ErrorMessage="Longitud maxima 50")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage="Campo Obligatorio")]
+        [StringLength(50, ErrorMessage="Longitud maxima 50")]
         public string Apellidos { get; set; }
+        [Display(Name="Numero Telefonico")]
         public string NumeroTelefono { get; set; }
         // Genero de la persona
         public Genero Genero { set; get; }
