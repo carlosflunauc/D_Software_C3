515fcf570e3cea43f663e2aa28511d5f4c0b5c38
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-12 16:07:27 -0500

    refactorizacion entidades apra campos requeridos en el frontend
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- HospiEnCasa.App.Dominio/Entidades/Persona.cs
+++ HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
+    /// <summary>Class <c>Persona</c>
+     /// Modela una Persona en general en el sistema 
+     /// </summary> 
     public class Persona
     {
         // Identificador único de cada persona
         public int Id { get; set; }
+        [Required(ErrorMessage="Campo Obligatorio")]
+        [StringLength(50, ErrorMessage="Longitud Maxima 50")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage="Campo Obligatorio")]
+        [StringLength(50, ErrorMessage="Longitud Maxima 50")] 
         public string Apellidos { get; set; }
+        [Required(ErrorMessage="Campo Obligatorio")]
+        [StringLength(20, ErrorMessage="Longitud Maxima 20")]
         public string NumeroTelefono { get; set; }
         // Genero de la persona
         public Genero Genero { set; get; }
